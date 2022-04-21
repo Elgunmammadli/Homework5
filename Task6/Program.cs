7022b539054f431760c71fa1e7ea7f0fd1f0a26f
Elgun <elgunam@code.edu.az>
2022-04-21 22:41:02 +0400

    done

---
+++ Task6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               6) Verilmish metnde {a} simvolunun sol terefden ve sag terefden indexleri eydidirmi?
+             */
+
+            Console.Write("Metn daxil et: ");
+            string sentence = Convert.ToString(Console.ReadLine());
+            char[] forA = { 'A', 'a' };
+
+            int index1 = sentence.IndexOfAny(forA, 0);//3 for n
+            int index2 = (sentence.Length-1) - index1;
+            
+            if (index1==index2)
+            {
+                Console.WriteLine("eynidir");
+            }
+            else
+            {
+                Console.WriteLine("eyni deyil");
+            }
+        }
+    }
+}
