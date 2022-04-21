2dedbcd9297e7bb2778ab3f702bd67271b8a2ab9
Elgun <elgunam@code.edu.az>
2022-04-21 21:48:17 +0400

    done

---
+++ Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               4) Verilmish metnde sol terefden ilk rast gelinen {a} simvolunun yeri tek ededdi yoxsa cut ?
+             */
+
+            Console.Write("Metn daxil et: ");
+            string sentence = Convert.ToString(Console.ReadLine());
+
+            int index = sentence.IndexOf('a');
+
+            if (index % 2 == 0)
+            {
+                Console.WriteLine("ilk 'a' simvolu tek yerdedir : {0} ci yer",index + 1);
+            }
+            else
+            {
+                Console.WriteLine("ilk 'a' simvolu cut yerdedir : {0} ci yer", index + 1);
+            }
+        }
+    }
+}
