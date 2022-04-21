88ce14ebec3f4d645cc84e450a3e4c88b25fed6d
Elgun <elgunam@code.edu.az>
2022-04-21 19:07:16 +0400

    done

---
+++ Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             2) Verilmish metnde sol terefden tek yerde dayanan simvollarin hamisi {a} simvoludurmu?
+             */
+            Console.Write("Metn daxil et : ");
+            string sentence = Convert.ToString(Console.ReadLine()); //"ana ata";
+            int countA = 0;
+            int countOddIndex = (sentence.Length+1)/2;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (i%2==0)
+                {
+                    if (sentence[i].ToString().ToUpper()=="A")
+                    {
+                        countA++;
+                    }
+                }
+            }
+            if (countOddIndex == countA)
+            {
+                Console.WriteLine("hamisi a simvoludur");
+            }
+            else {
+                Console.WriteLine("hamisi a simvolu deyil");
+            }
+        }
+    }
+}
