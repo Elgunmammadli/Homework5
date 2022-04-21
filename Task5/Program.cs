f0eecf52fb411c62f839f93a5b82c2380c28883a
Elgun <elgunam@code.edu.az>
2022-04-21 21:59:17 +0400

    done

---
+++ Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             5) Verilmish metnde sol terefden saydiqda {a},{b},{c} simollarindan hansi birinci gelir?
+             */
+
+            Console.Write("Metn daxil et: ");
+            string sentence = Convert.ToString(Console.ReadLine());
+
+            char[] chars = { 'a', 'b', 'c' };
+            int index = sentence.IndexOfAny(chars,0);
+
+            Console.WriteLine($"'{sentence[index]}' birinci gelir. yeri: {index+1} ");
+        }
+    }
+}
