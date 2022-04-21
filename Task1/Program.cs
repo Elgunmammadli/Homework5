b27338e346f7c948dc444c8ab76325c99e7baea0
Elgun <elgunam@code.edu.az>
2022-04-21 18:27:20 +0400

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               1) Verilmish metnde {a} simvolun sayi {b} simvolun sayinda nece defe coxdur?
+             */
+
+            string sentence = "slm bb";
+
+            int countA = 0;
+            int countB = 0;
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i].ToString().ToUpper() == "A")
+                {
+                    countA++;
+                }
+                else if (sentence[i].ToString().ToUpper() == "B")
+                {
+                    countB++;
+                }
+            }
+            if (countA==0 )
+            {
+                Console.WriteLine($"a simvolu yoxdur b simvolu sayi :{countB}");
+                return;
+            }
+            else if (countB == 0)
+            {
+                Console.WriteLine($"b simvolu yoxdur a simvolu sayi :{countA}");
+                return;
+            }
+            double qismet = countA / countB;
+            Console.WriteLine("a simvollarinin sayi ,b simvollarin sayindan {0} defe choxdur",qismet);
+        }
+    }
+}
