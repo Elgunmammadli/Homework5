1b4d2159f959c0634b0d16bcbf0bfe6c8a73ea75
Elgun <elgunam@code.edu.az>
2022-04-22 00:25:34 +0400

    done

---
+++ Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              10) Verilmish metinde butun reqemleri legv et. 
+             */
+
+            Console.Write("Metn daxil et: ");
+            string sentence = Convert.ToString(Console.ReadLine());
+
+            string newSentence = Regex.Replace(sentence, @"\d", "");
+
+            Console.WriteLine(newSentence);
+        }
+    }
+}
