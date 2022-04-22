078fa3e7981f908826ee5716697c4047058af528
Elgun <elgunam@code.edu.az>
2022-04-22 22:48:31 +0400

    done

---
+++ Task13/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace Task13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              13) Verilimish metinde butun simvollari ardicil shekilde biri balaca, biri boyuk formada cap et. 
+             */
+
+            Console.Write("Metn daxil et: ");
+            string sentence = Convert.ToString(Console.ReadLine());
+            string temp = sentence;
+            sentence = sentence.ToLower();
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (i%2!=0)
+                {
+                    sentence = sentence.Remove(i,1);
+                    sentence = sentence.Insert(i, temp[i].ToString().ToUpper());
+                }
+            }
+            Console.WriteLine(sentence);
+        }
+    }
+}
