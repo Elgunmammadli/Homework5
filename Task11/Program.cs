9d6199d9fc36deaeccc49e347b35ed0d2a4c24c7
Elgun <elgunam@code.edu.az>
2022-04-22 18:30:45 +0400

    done

---
+++ Task11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              11) Verilmish metinde butun {a} simvollarinin qabagina {b} simvolunu ve
+              eyni zamandan butun {b} simvollarinin qabagina {a} simvolunu yaz. 
+             */
+
+            Console.Write("Metn daxil et: ");
+            string sentence = Convert.ToString(Console.ReadLine());
+
+            string newSen = sentence.Replace("a","ab");
+            string newSenn = newSen.Replace("b", "ba");
+
+            Console.WriteLine(newSenn);
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
